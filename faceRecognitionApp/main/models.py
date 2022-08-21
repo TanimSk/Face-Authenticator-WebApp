9b9ed5893d96ffa14db4ec7d0c3ee0cdb9047bb6
tanimsk <sktanim5800@gmail.com>
2022-08-22 00:11:33 +0600

    db added

--- faceRecognitionApp/main/models.py
+++ faceRecognitionApp/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Registered_user(models.Model):
+    name = models.CharField(max_length=50, default='Not Given')
+    email = models.CharField(max_length=100, default='Not Given')
+
+class User_image(models.Model):
+    image = models.ImageField()
+    user = models.ForeignKey(Registered_user, on_delete=models.CASCADE)
--- faceRecognitionApp/main/views.py
+++ faceRecognitionApp/main/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 def form(req):
     return render(req, 'form.html')
 
+
 def register(req):
+    if req.method == 'POST':
+        name = req.POST.get('user')
+        email = req.POST.get('email')
+        
+        print(req.FILES.get('images'), name, email)
+        HttpResponse(False)
+
     return render(req, 'register.html')
